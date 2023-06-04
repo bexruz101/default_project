a1f5f2e2592d2f39b4d403c688876f41cf0eb414
Dunistan <Bexruz>
2023-06-04 15:39:13 +0500

    Initial commit

---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColor{
+  static const Color c_407AFF = Color(0xFF407AFF);
+  static const Color c_E5E5E5 = Color(0xFFE5E5E5);
+  static const Color c_87898E = Color(0xFF87898E);
+
+}
---
+++ lib/ui/utils/images.dart
@@ -0,0 +1,9 @@
+class im{
+
+  //png
+  static final String p1 = 'assets/png/p1.png';
+  static final String p2 = 'assets/png/p2.png';
+
+  //svg
+  static final String lupa = 'assets/svg/lupa.svg';
+}
