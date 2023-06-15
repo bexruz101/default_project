92f1793c9c2daa3921c01919c895b11120b5b625
bexruz101 <bexruzyusupov2006@gmail.com>
2023-06-15 21:22:38 +0500

    halit tugamadi onsubmitdi kontrolerga ozgrartish kerak

--- lib/ui/widgets/input_text.dart
+++ lib/ui/widgets/input_text.dart
@@ -9,26 +9,11 @@ import '../../utils/colors.dart';
 import 'input_text.dart';
 
 class InputText extends StatelessWidget {
-  const InputText({super.key, required this.mainText, required this.hintText, required this.type});
-
-  static late  SharedPreferences preferences;
-  Future<void> _init() async{
-    preferences = await  SharedPreferences.getInstance();
-  }
-
-  TextEditingController _name = TextEditingController();
-  TextEditingController _email = TextEditingController();
-  TextEditingController _birthdata = TextEditingController();
-  TextEditingController _phone = TextEditingController();
-  TextEditingController _password = TextEditingController();
-
+   InputText({super.key, required this.mainText, required this.hintText, required this.type});
 
   final String mainText;
   final String hintText;
   final TextInputType type;
-
-  static late Map<ProfileModel,dynamic> data;
-  static late ProfileModel value2;
 
   @override
   Widget build(BuildContext context) {
@@ -39,9 +24,6 @@ class InputText extends StatelessWidget {
           Text(mainText,style: TextStyle(fontWeight: FontWeight.w400,fontSize: 15),),
           SizedBox(height: 10,),
           TextField(
-            onSubmitted: (value){
-              mainText=='Name'?value2.name = value:mainText == 'Email'?value2.email=value:mainText == 'Data of Birth'?value2.dataOfBirth = value:mainText=='Phone Number'?value2.phoneNumber = value:value2.password=value;
-            },
             maxLines: mainText==tr('Address')?4:1,
             style: TextStyle(fontSize: 15,fontWeight: FontWeight.w400),
            keyboardType: type,
@@ -58,18 +40,7 @@ class InputText extends StatelessWidget {
     );
   }
 
-  void _saveData()async{
-    String userData = jsonEncode(value2);
-    print(userData);
-    preferences.setString('userData', userData);
-  }
-  void _getData(){
-    Map<String,dynamic> getDatas = jsonDecode(preferences.getString('userData')!);
-    ProfileModel mod =  ProfileModel.fromJson(getDatas);
-    if(getDatas.isNotEmpty){
-       = TextEditingValue(text: value2.name);
-    }
-  }
+
 
 
 
