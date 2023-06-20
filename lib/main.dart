4a0fe02c905438c71ee070081fb48e8caa5c3c16
bexruz101 <bexruzyusupov2006@gmail.com>
2023-06-20 12:12:05 +0500

    master

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       home: HomeScreen(),
     );
--- lib/ui/utils/images.dart
+++ lib/ui/utils/images.dart
@@ -2,7 +2,11 @@ class AppImages{
 
   //png
   static final String p1 = 'assets/png/p1.png';
-  static final String p2 = 'assets/png/p2.png';
+
+
+  //jpg
+  static final String p2 = 'assets/png/p2.jpg';
+
 
   //svg
   static final String lupa = 'assets/svg/lupa.svg';
